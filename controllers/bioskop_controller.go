26c8368cc3d66291157ed0ece632615323653d78
adhityadrp <adhityadrp@gmail/com>
2025-10-31 05:36:34 +0700

    feat: Tugas Pekan 3, Hari 14, Adhitya DRP

--- controllers/bioskop_controller.go
+++ controllers/bioskop_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//CREATE BY ID
 func CreateBioskop(c *gin.Context) {
 	var input models.Bioskop
 
@@ -25,8 +26,69 @@ func CreateBioskop(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Bioskop berhasil ditambahkan", "data": input})
 }
 
+// GET ALL
 func GetAllBioskop(c *gin.Context) {
-	var bioskops []models.Bioskop
-	config.DB.Find(&bioskops)
-	c.JSON(http.StatusOK, gin.H{"data": bioskops})
+	var bioskop []models.Bioskop
+	result := config.DB.Find(&bioskop)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, bioskop)
+}
+
+// GET BY ID
+func GetBioskopByID(c *gin.Context) {
+	id := c.Param("id")
+	var bioskop models.Bioskop
+
+	if err := config.DB.First(&bioskop, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bioskop tidak ditemukan"})
+		return
+	}
+
+	c.JSON(http.StatusOK, bioskop)
+}
+
+// UPDATE
+func UpdateBioskop(c *gin.Context) {
+	id := c.Param("id")
+	var bioskop models.Bioskop
+
+	if err := config.DB.First(&bioskop, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bioskop tidak ditemukan"})
+		return
+	}
+
+	var input models.Bioskop
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Data tidak valid"})
+		return
+	}
+
+	if input.Nama == "" || input.Lokasi == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Nama dan lokasi wajib diisi"})
+		return
+	}
+
+	bioskop.Nama = input.Nama
+	bioskop.Lokasi = input.Lokasi
+	bioskop.Rating = input.Rating
+
+	config.DB.Save(&bioskop)
+	c.JSON(http.StatusOK, gin.H{"message": "Data bioskop berhasil diperbarui", "data": bioskop})
+}
+
+// DELETE
+func DeleteBioskop(c *gin.Context) {
+	id := c.Param("id")
+	var bioskop models.Bioskop
+
+	if err := config.DB.First(&bioskop, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bioskop tidak ditemukan"})
+		return
+	}
+
+	config.DB.Delete(&bioskop)
+	c.JSON(http.StatusOK, gin.H{"message": "Bioskop berhasil dihapus"})
 }
--- routes/route.go
+++ routes/route.go
@@ -11,6 +11,9 @@ func SetupRouter() *gin.Engine {
 
 	r.POST("/bioskop", controllers.CreateBioskop)
 	r.GET("/bioskop", controllers.GetAllBioskop)
+	r.GET("/bioskop/:id", controllers.GetBioskopByID)
+	r.PUT("/bioskop/:id", controllers.UpdateBioskop)
+	r.DELETE("/bioskop/:id", controllers.DeleteBioskop)
 
 	return r
 }
