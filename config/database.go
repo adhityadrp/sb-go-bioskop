53f9f1099483a6d868ec086ccc6ba45cdc4476df
adhityadrp <adhityadrp@gmail/com>
2025-10-31 21:37:39 +0700

    refactor for deploy on railway

--- config/database.go
+++ config/database.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,7 +11,45 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=postgres dbname=sanbercode_golang_bioskop port=5432 sslmode=disable"
+	// Get environment variables with fallback values
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "postgres"
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "postgres"
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "sanbercode_golang_bioskop"
+	}
+
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+	}
+
+	// Check if DATABASE_URL is provided (Railway specific)
+	dbUrl := os.Getenv("DATABASE_URL")
+	var dsn string
+
+	if dbUrl != "" {
+		// If DATABASE_URL is provided, use it directly
+		dsn = dbUrl
+	} else {
+		// Otherwise, construct the connection string from individual parameters
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			dbHost, dbUser, dbPassword, dbName, dbPort)
+	}
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Gagal koneksi ke database")
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"log"
+	"os"
 	"sb-go-bioskop/config"
 	"sb-go-bioskop/models"
 	"sb-go-bioskop/routes"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
 	config.ConnectDatabase()
 	config.DB.AutoMigrate(&models.Bioskop{})
 
 	r := routes.SetupRouter()
-	r.Run(":8080")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
 }
